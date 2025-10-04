ec405bec427f0802650c67d78449198eb7a7e09d
Allan Barbosa <allan.barbosa@yahoo.com.br>
2025-10-04 19:24:40 -0300

    Feat: Add investment management features, including investment creation and wallet operations; implement Pix in use validation

--- src/main/java/br/com/dio/repository/AccountRepository.java
+++ src/main/java/br/com/dio/repository/AccountRepository.java
@@ -1,6 +1,7 @@
 package br.com.dio.repository;
 
 import br.com.dio.exception.AccountNotFoundException;
+import br.com.dio.exception.PixInUseException;
 import br.com.dio.model.AccountWallet;
 
 import java.util.List;
@@ -12,24 +13,31 @@ public class AccountRepository {
     private List<AccountWallet> accounts;
 
     public AccountWallet create(final List<String> pix, final long initialAmount) {
+
+        var pixInUse = accounts.stream().flatMap(a -> a.getPix().stream()).toList();
+        for (var p : pix) {
+            if (pixInUse.contains(p)) {
+                throw new PixInUseException("Pix already in use: " + p);
+            }
+        }
         var newAccount = new AccountWallet(initialAmount, pix);
         accounts.add(newAccount);
         return newAccount;
     }
 
-    public void deposit(final String pix, final long fundsAmount){
+    public void deposit(final String pix, final long fundsAmount) {
         var account = findByPix(pix);
         account.addMoney(fundsAmount, "Deposit by PIX");
     }
 
-    public long withdraw(final String pix, final long amount){
+    public long withdraw(final String pix, final long amount) {
         var source = findByPix(pix);
         checkFundsForTransaction(source, amount);
         source.reduceMoney(amount);
         return amount;
     }
 
-    public void transferMoney (final String sourcePix, final String targetPix, final long amount){
+    public void transferMoney(final String sourcePix, final String targetPix, final long amount) {
         var source = findByPix(sourcePix);
         checkFundsForTransaction(source, amount);
         var target = findByPix(targetPix);
---
+++ src/main/java/br/com/dio/repository/InvestmentRepository.java
@@ -0,0 +1,84 @@
+package br.com.dio.repository;
+
+import br.com.dio.exception.AccountWithInvestmentException;
+import br.com.dio.exception.InvestmentNotFoundException;
+import br.com.dio.exception.WalletNotFoundException;
+import br.com.dio.model.AccountWallet;
+import br.com.dio.model.Investment;
+import br.com.dio.model.InvestmentWallet;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static br.com.dio.repository.CommonsRepository.checkFundsForTransaction;
+
+public class InvestmentRepository {
+
+    private long nextId;
+    private final List<Investment> investments = new ArrayList<>();
+    private final List<InvestmentWallet> wallets = new ArrayList<>();
+
+    public Investment create(final long tax, final long initialFunds) {
+        this.nextId++;
+        var investment = new Investment(this.nextId, tax, initialFunds);
+        this.investments.add(investment);
+        return investment;
+    }
+
+    public InvestmentWallet newInvestment(final AccountWallet account, final long id) {
+        var accountInUse = wallets.stream().map(InvestmentWallet::getAccount).toList();
+            if (accountInUse.contains(account)) {
+                throw new AccountWithInvestmentException("Account '" + account + "' already has an investment wallet.");
+            }
+
+        var investment = findById(id);
+        checkFundsForTransaction(account, investment.initialFunds());
+        var wallet = new InvestmentWallet(investment, account, investment.initialFunds());
+        this.wallets.add(wallet);
+        return wallet;
+    }
+
+    public InvestmentWallet deposit(final String pix, final long funds) {
+        var wallet = findWalletByAccountPix(pix);
+        wallet.addMoney(wallet.getAccount().reduceMoney(funds), wallet.getService(), "Deposit to investment");
+        return wallet;
+    }
+
+    public InvestmentWallet withdraw(final String pix, final long funds) {
+        var wallet = findWalletByAccountPix(pix);
+        checkFundsForTransaction(wallet, funds);
+        wallet.getAccount().addMoney(wallet.reduceMoney(funds), wallet.getService(), "Withdraw from investment");
+        if (wallet.getFunds() == 0) {
+            this.wallets.remove(wallet);
+        }
+        return wallet;
+    }
+
+    public void updateAmount(final long percent) {
+        this.wallets.forEach(w -> w.updateAmount(percent));
+    }
+
+    public Investment findById(final long id) {
+        return this.investments.stream()
+                .filter(i -> i.id() == id)
+                .findFirst()
+                .orElseThrow(() -> new InvestmentNotFoundException("Investment not found for id: " + id));
+    }
+
+    public InvestmentWallet findWalletByAccountPix(final String pix) {
+        return this.wallets.stream()
+                .filter(w -> w.getAccount().getPix().contains(pix))
+                .findFirst()
+                .orElseThrow(() -> new WalletNotFoundException("Wallet not found for pix: " + pix));
+    }
+
+    public List<Investment> list() {
+        return this.investments;
+    }
+
+    public List<InvestmentWallet> listWallets() {
+        return this.wallets;
+    }
+
+
+}
