5260079110d43ff392c3ac7910aec77f233777bb
Allan Barbosa <allan.barbosa@yahoo.com.br>
2025-10-06 03:16:24 -0300

    wip: implement main and submenus structure with placeholders

--- src/main/java/br/com/dio/Main.java
+++ src/main/java/br/com/dio/Main.java
@@ -13,107 +13,133 @@ public class Main {
         InvestmentRepository investmentRepository = new InvestmentRepository();
 
         System.out.println("Welcome to the Dio-Bank, the official bank of DIO.");
-        int option;
+        int option = -1;
 
         do {
             System.out.println("\n## Dio-Bank ##\n");
             System.out.println("Please choose an option:");
-            System.out.println("1. Account");
-            System.out.println("2. Investments");
-            System.out.println("3. Search");
-            System.out.println("0. Exit\n");
+            System.out.println("1) Accounts");
+            System.out.println("2) Investments");
+            System.out.println("3) Search");
+            System.out.println("0) Exit\n");
+            System.out.print("Option: ");
+
+            if (!sc.hasNextInt()) {
+                System.out.println("Invalid input. Please enter a number.");
+                sc.next();
+                continue;
+            }
+
             option = sc.nextInt();
             sc.nextLine();
 
             switch (option) {
-                case 1:
-                    System.out.println("Account selected.");
-                    System.out.println("Please choose an option:\n");
-                    System.out.println("[1] Create Account");
-                    System.out.println("[2] Deposit");
-                    System.out.println("[3] Withdraw");
-                    System.out.println("[4] Transfer");
-                    System.out.println("[5] List All Accounts");
-                    System.out.println("[0] Back to Main Menu");
+                case 1 -> {
+                    int accountOption = -1;
 
-                    final int accountOption = sc.nextInt();
-                    sc.nextLine();
+                    do {
+                        System.out.println("\n=== ACCOUNT MENU ===");
+                        System.out.println("[1] Create Account");
+                        System.out.println("[2] Deposit");
+                        System.out.println("[3] Withdraw");
+                        System.out.println("[4] Transfer");
+                        System.out.println("[5] List All Accounts");
+                        System.out.println("[0] Back to Main Menu");
+                        System.out.print("Option: ");
 
-                    if (accountOption == 1) {
-                    } else if (accountOption == 2) {
-                    } else if (accountOption == 3) {
-                    } else if (accountOption == 4) {
-                    } else if (accountOption == 5) {
-                    } else if (accountOption == 0) {
-                        System.out.println("Returning to main menu...");
-                    } else {
-                        System.out.println("Invalid option. Please try again.");
-                    }
-                    break;
+                        if (!sc.hasNextInt()) {
+                            System.out.println("Invalid input. Please enter a number.");
+                            sc.next();
+                            continue;
+                        }
 
-                case 2:
-                    System.out.println("Investments selected.");
-                    System.out.println("Please choose an option:\n");
-                    System.out.println("[1] Create Investment Product");
-                    System.out.println("[2] Open Investment Wallet");
-                    System.out.println("[3] Deposit to Investment");
-                    System.out.println("[4] Withdraw from Investment");
-                    System.out.println("[5] Apply Earnings Update");
-                    System.out.println("[6] List Investment Products");
-                    System.out.println("[7] List Investment Wallets");
-                    System.out.println("[0] Back to Main Menu");
+                        accountOption = sc.nextInt();
+                        sc.nextLine();
 
-                    final int investmentOption = sc.nextInt();
-                    sc.nextLine();
+                        switch (accountOption) {
+                            case 1 -> System.out.println("Creating account... (placeholder)");
+                            case 2 -> System.out.println("Depositing... (placeholder)");
+                            case 3 -> System.out.println("Withdrawing... (placeholder)");
+                            case 4 -> System.out.println("Transferring... (placeholder)");
+                            case 5 -> System.out.println("Listing all accounts... (placeholder)");
+                            case 0 -> System.out.println("Returning to main menu...");
+                            default -> System.out.println("Invalid option. Please try again.");
+                        }
+                    } while (accountOption != 0);
+                }
 
-                    if (investmentOption == 1) {
-                    } else if (investmentOption == 2) {
-                    } else if (investmentOption == 3) {
-                    } else if (investmentOption == 4) {
-                    } else if (investmentOption == 5) {
-                    } else if (investmentOption == 6) {
-                    } else if (investmentOption == 7) {
-                    } else if (investmentOption == 0) {
-                        System.out.println("Returning to main menu...");
-                    } else {
-                        System.out.println("Invalid option. Please try again.");
-                    }
-                    break;
+                case 2 -> {
+                    int investmentOption = -1;
 
-                case 3:
-                    System.out.println("Search selected.");
-                    System.out.println("Please choose an option:\n");
-                    System.out.println("[1] Find Account by PIX");
-                    System.out.println("[2] Find Investment Wallet by PIX");
-                    System.out.println("[3] View Account Transactions");
-                    System.out.println("[0] Back to Main Menu");
-                    final int searchOption = sc.nextInt();
-                    sc.nextLine();
+                    do {
+                        System.out.println("\n=== INVESTMENT MENU ===");
+                        System.out.println("[1] Create Investment Product");
+                        System.out.println("[2] Open Investment Wallet");
+                        System.out.println("[3] Deposit to Investment");
+                        System.out.println("[4] Withdraw from Investment");
+                        System.out.println("[5] Apply Earnings Update");
+                        System.out.println("[6] List Investment Products");
+                        System.out.println("[7] List Investment Wallets");
+                        System.out.println("[0] Back to Main Menu");
+                        System.out.print("Option: ");
+                        if (!sc.hasNextInt()) {
+                            System.out.println("Invalid input. Please enter a number.");
+                            sc.next();
+                            continue;
+                        }
 
-                    if (searchOption == 1) {
-                    } else if (searchOption == 2) {
-                    } else if (searchOption == 3) {
-                    } else if (searchOption == 0) {
-                        System.out.println("Returning to main menu...");
-                    } else {
-                        System.out.println("Invalid option. Please try again.");
-                    }
-                    break;
+                        investmentOption = sc.nextInt();
+                        sc.nextLine();
 
-                case 0:
-                    System.out.println("Thank you for using our services. See you next time!");
-                    break;
+                        switch (investmentOption) {
+                            case 1 -> System.out.println("Creating investment product... (placeholder)");
+                            case 2 -> System.out.println("Opening investment wallet... (placeholder)");
+                            case 3 -> System.out.println("Depositing to investment... (placeholder)");
+                            case 4 -> System.out.println("Withdrawing from investment... (placeholder)");
+                            case 5 -> System.out.println("Applying earnings update... (placeholder)");
+                            case 6 -> System.out.println("Listing investment products... (placeholder)");
+                            case 7 -> System.out.println("Listing investment wallets... (placeholder)");
+                            case 0 -> System.out.println("Returning to main menu...");
+                            default -> System.out.println("Invalid option. Please try again.");
+                        }
+                    } while (investmentOption != 0);
+                }
 
-                default:
-                    System.out.println("Invalid option. Please try again.");
-                    break;
+                case 3 -> {
+                    int searchOption = -1;
+
+                    do {
+                        System.out.println("\n=== SEARCH MENU ===");
+                        System.out.println("[1] Find Account by PIX");
+                        System.out.println("[2] Find Investment Wallet by PIX");
+                        System.out.println("[3] View Account Transactions");
+                        System.out.println("[0] Back to Main Menu");
+                        System.out.print("Option: ");
+
+                        if (!sc.hasNextInt()) {
+                            System.out.println("Invalid input. Please enter a number.");
+                            sc.next();
+                            continue;
+                        }
+
+                        searchOption = sc.nextInt();
+                        sc.nextLine();
+
+                        switch (searchOption) {
+                            case 1 -> System.out.println("Finding account by PIX... (placeholder)");
+                            case 2 -> System.out.println("Finding investment wallet by PIX... (placeholder)");
+                            case 3 -> System.out.println("Viewing account transactions... (placeholder)");
+                            case 0 -> System.out.println("Returning to main menu...");
+                            default -> System.out.println("Invalid option. Please try again.");
+                        }
+                    } while (searchOption != 0);
+                }
+                case 0 -> System.out.println("Thank you for using our services. See you next time!");
+                default -> System.out.println("Invalid option. Please try again.");
             }
-        } while (option != 0);
 
+        } while (option != 0);
 
         sc.close();
     }
 }
-
-
-
